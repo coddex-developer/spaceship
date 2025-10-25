36ae4dd383d2e93909d92a069a4aa4205aa0de58
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2025-10-24 23:28:12 -0300

    adicionar carregamento ao componente Models e remover componente Loading

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -2,10 +2,11 @@
 import { data } from "@/db/db"
 import { spaceShipModel } from "@/db/spaceshipsType"
 import Link from "next/link"
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import "./style.min.scss";
 
 export default function Models() {
+    const [loading, setLoading] = useState(true)
     type Accum = {
         [key: string]: {
             category: string,
@@ -24,42 +25,63 @@ export default function Models() {
             accum[category].itens.push(item);
             return accum;
         }, {})
-    }, [data])
+    }, [])
 
     const allCategory = Object.values(categoryItems)
+
+    useEffect(() => {
+
+        const time = setTimeout(() => {
+            setLoading(false)
+        }, 1500)
+
+        return () => clearTimeout(time)
+    }, [])
+
     return (
         <>
-            <div className="w-full px-6 flex flex-col justify-center items-center h-dvh gap-4">
-                <h1 className="text-center font-bold bg-gray py-3.5 text-[19pt]">Navegue por categoria</h1>
-                <table className="w-full lg:max-w-[600px]">
-                    <thead>
-                        <tr>
-                            <th className="dark:bg-blue-800 bg-blue-500 text-white">Categoria</th>
-                            <th className="dark:bg-blue-800 bg-blue-500 text-white">Quantidade</th>
-                            <th className="dark:bg-blue-800 bg-blue-500 text-white">Visualizar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
 
-                        {
-                            allCategory.map(item => (
-                                <tr key={item.category}>
-                                    <td className="dark:bg-gray-700 bg-blue-100">
-                                        {item.category}
-                                    </td>
-                                    <td className="dark:bg-gray-700 bg-blue-100">
-                                        {item.itens.length}
-                                    </td>
-                                    <td className="hover:bg-blue-500 dark:bg-gray-700 bg-blue-100" >
-                                        <Link className="w-full h-full block hover:text-white" href={`/models/${item.category}`}>Ver {item.category}</Link>
-                                    </td>
+            {
+                loading ? (
+                    <div className="flex justify-center w-full lg:max-w-[600px] h-dvh">
+                        <p className="text-xl font-semibold animate-pulse mt-48 text-gray-700 dark:text-gray-300">
+                            Carregando categorias...
+                        </p>
+                    </div>
+                ) : (
+                    <div className="w-full px-6 flex flex-col justify-center items-center h-dvh gap-4">
+                        <h1 className="text-center font-bold bg-gray py-3.5 text-[19pt]">Navegue por categoria</h1>
+                        <table className="w-full lg:max-w-[600px]">
+                            <thead>
+                                <tr>
+                                    <th className="dark:bg-blue-800 bg-blue-500 text-white">Categoria</th>
+                                    <th className="dark:bg-blue-800 bg-blue-500 text-white">Quantidade</th>
+                                    <th className="dark:bg-blue-800 bg-blue-500 text-white">Visualizar</th>
                                 </tr>
-                            ))
-                        }
+                            </thead>
+                            <tbody>
 
-                    </tbody>
-                </table>
-            </div>
+                                {
+                                    allCategory.map(item => (
+                                        <tr key={item.category}>
+                                            <td className="dark:bg-gray-700 bg-blue-100">
+                                                {item.category}
+                                            </td>
+                                            <td className="dark:bg-gray-700 bg-blue-100">
+                                                {item.itens.length}
+                                            </td>
+                                            <td className="hover:bg-blue-500 dark:bg-gray-700 bg-blue-100" >
+                                                <Link className="w-full h-full block hover:text-white" href={`/models/${item.category}`}>Ver {item.category}</Link>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
         </>
     )
 }
--- src/app/models/spaceship/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-    (
-        <>
-            <div className="w-full h-dvh bg-red-900 text-white flex items-center justify-center">
-                <p>Carregando informações aguarde...</p>
-            </div>
-        </>
-    )
-}
